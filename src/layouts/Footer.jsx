8c9d41ea801f73a39903dac03982203648dfa4e2
zeroskill2400 <zeroskill2400@zeroskill2400ui-MacBookPro.local>
2025-06-22 00:41:09 +0900

    레이아웃 제작

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My Community. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>커뮤니티</h1>
+      <nav>
+        <a href="/">홈</a>
+        <a href="/posts">게시판</a>
+        <a href="/login">로그인</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
