56438edd2ac97c1fc865f89c77c147545a6a5fe2
최호균/클라우드플랫폼개발팀 <hosi153@lguplus.co.kr>
2025-07-09 13:19:57 +0900

    구글 로그인 지원

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import "./index.css";
 import Layout from "./layouts/Layout";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { supabase } from "./libs/supabase";
+import { useUserStore } from "./stores/userStore";
 
 function App() {
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.user) {
+        useUserStore.getState().setUser(session.user);
+      } else {
+        useUserStore.getState().clearUser();
+      }
+    });
+  }, []);
+
   return (
     <Layout>
       <Outlet />
